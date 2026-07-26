6731a2404cc8e1f32beadccbcaaba356f5b87a05
Daniel Rosemann <daniel.rosemann10@gmail.com>
2026-07-26 13:33:50 +0200

    rename mesh.py to mesh_networking.py

--- cli/ipilot/commands/edge_computing/__init__.py
+++ cli/ipilot/commands/edge_computing/__init__.py
@@ -5,8 +5,8 @@ from .edge_computing import app as edge_computing_app
 from .edge_functions import app as edge_functions_app
 from .internet_of_things import app as internet_of_things_app
 from .lora_wan import app as lora_wan_app
-from .mesh import app as mesh_app
+from .mesh_networking import app as mesh_networking_app
 from .ml import app as ml_app
 from .pipeline import app as pipeline_app
 
-__all__ = ["content_delivery_network_app", "edge_computing_app", "edge_functions_app", "internet_of_things_app", "lora_wan_app", "mesh_app", "ml_app", "pipeline_app"]
+__all__ = ["content_delivery_network_app", "edge_computing_app", "edge_functions_app", "internet_of_things_app", "lora_wan_app", "mesh_networking_app", "ml_app", "pipeline_app"]

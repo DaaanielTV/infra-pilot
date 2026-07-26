7675c6604a90d52eccaeaebd8d44361ef3df573a
Daniel Rosemann <daniel.rosemann10@gmail.com>
2026-07-26 13:27:06 +0200

    rename dres.py to data_residency.py

--- cli/ipilot/commands/__init__.py
+++ cli/ipilot/commands/__init__.py
@@ -224,7 +224,7 @@ from .compliance_v2.attestation_reports import app as attestation_reports_app
 from .compliance_v2.vcom import app as vcom_app
 from .compliance_v2.regintel import app as regintel_app
 from .compliance_v2.audit_management import app as audit_management_app
-from .compliance_v2.dres import app as dres_app
+from .compliance_v2.data_residency import app as data_residency_app
 from .compliance_v2.train import app as train_app
 from .compliance_v2.auditor_portal import app as auditor_portal_app
 
@@ -235,7 +235,7 @@ register("attest", "Attestation")(attestation_reports_app)
 register("vcom", "Vendor compliance")(vcom_app)
 register("regintel", "Regulatory intel")(regintel_app)
 register("audit-mgmt", "Audit management")(audit_management_app)
-register("dres", "Data residency")(dres_app)
+register("dres", "Data residency")(data_residency_app)
 register("train", "Compliance training")(train_app)
 register("auditor", "Auditor portal")(auditor_portal_app)
 
@@ -308,7 +308,7 @@ __all__: list[str] = [
     "templatereg_app", "techdebt_app", "environments_app", "apicatalog_app",
     "docgen_app", "pulse_app",
     "continuous_compliance_app", "evidence_app", "compliance_as_code_app", "attestation_reports_app", "vcom_app",
-    "regintel_app", "audit_management_app", "dres_app", "train_app", "auditor_portal_app",
+    "regintel_app", "audit_management_app", "data_residency_app", "train_app", "auditor_portal_app",
     "blockchain_app", "storage_app", "quantum_app", "contracts_app",
     "web3id_app", "confidential_app", "federated_app", "zkp_app", "dcn_app",
     "dr_app", "active_active_app", "backup_sla_app", "chaos_exp_app",

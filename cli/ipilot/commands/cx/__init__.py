7dd7bf1b1799dca9cce040e684694b3571c71484
Daniel Rosemann <daniel.rosemann10@gmail.com>
2026-07-26 13:29:17 +0200

    rename canned.py to canned_responses.py

--- cli/ipilot/commands/cx/__init__.py
+++ cli/ipilot/commands/cx/__init__.py
@@ -5,7 +5,7 @@ app = typer.Typer(help="Customer experience")
 from .health import app as cx_health_app
 from .ticket import app as cx_ticket_app
 from .sla import app as cx_sla_app
-from .canned import app as canned_app
+from .canned_responses import app as canned_responses_app
 from .sentiment import app as sentiment_app
 from .adoption import app as adoption_app
 from .onboarding import app as onboarding_app
@@ -18,7 +18,7 @@ from .success import app as success_app
 app.add_typer(cx_health_app, name="health")
 app.add_typer(cx_ticket_app, name="ticket")
 app.add_typer(cx_sla_app, name="sla")
-app.add_typer(canned_app, name="canned")
+app.add_typer(canned_responses_app, name="canned")
 app.add_typer(sentiment_app, name="sentiment")
 app.add_typer(adoption_app, name="adoption")
 app.add_typer(onboarding_app, name="onboarding")

807083f0ef8cbf736fe5ba8a4d48b9ac9644203f
Daniel Rosemann <daniel.rosemann10@gmail.com>
2026-07-26 13:30:57 +0200

    rename cdn.py to content_delivery_network.py

--- cli/ipilot/commands/edge_computing/__init__.py
+++ cli/ipilot/commands/edge_computing/__init__.py
@@ -1,6 +1,6 @@
 """Edge computing commands."""
 
-from .cdn import app as cdn_app
+from .content_delivery_network import app as content_delivery_network_app
 from .edge import app as edge_app
 from .functions import app as fn_app
 from .iot import app as iot_app
@@ -9,4 +9,4 @@ from .mesh import app as mesh_app
 from .ml import app as ml_app
 from .pipeline import app as pipeline_app
 
-__all__ = ["cdn_app", "edge_app", "fn_app", "iot_app", "gw_app", "mesh_app", "ml_app", "pipeline_app"]
+__all__ = ["content_delivery_network_app", "edge_app", "fn_app", "iot_app", "gw_app", "mesh_app", "ml_app", "pipeline_app"]
